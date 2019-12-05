7800d360c8049501fbc3841cee3c4b4106a76910
Mantas1A <mantasas15@gmail.com>
2019-12-06 01:04:22 +0200

    ddd

---
+++ resources/views/dog/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+Dogger
+    <div class="">
+        <img src="/images/img/home/10.jpg" alt="">
+    </div>
+
+    <div class="">
+        <h1>Prekės šunims</h1>
+    </div>
+
+    <div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="">
+                    <img src="/images/img/button/10.png" alt="">
+                </div>
+                <div>
+                    <h5>Gyvunėliai</h5>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="">
+                    <img src="/images/img/button/4.png" alt="">
+                </div>
+                <div>
+                    <h5>Maistas</h5>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="">
+                    <img src="/images/img/button/7.png" alt="">
+                </div>
+                <div>
+                    <h5>Tualetai</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="">
+                    <img src="/images/img/button/8.png" alt="">
+                </div>
+                <div>
+                    <h5>Žaislai</h5>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="">
+                    <img src="/images/img/button/14.png" alt="">
+                </div>
+                <div>
+                    <h5>Aksesuarai</h5>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="">
+                    <img src="/images/img/button/9.png" alt="">
+                </div>
+                <div>
+                    <h5>Guoliai</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
