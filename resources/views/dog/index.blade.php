e684615edc92b95bd6d48747316edad658e86630
cozy8 <ingiukasgudz@gmail.com>
2019-12-10 14:54:35 +0200

    sjd

--- resources/views/dog/index.blade.php
+++ resources/views/dog/index.blade.php
@@ -1,69 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-Dogger
-    <div class="">
-        <img src="/images/img/home/10.jpg" alt="">
-    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class= "rudasfonas">
+                <div class="topatitraukimas">
+                    <img src="/images/img/home/9.jpg" alt="">
+                </div>
+                <div class="topatitraukimas">
+                    <h2>Prekės šunims</h2>
+                </div>
+            </div>
+                <div class="rudasfonas">
+                    <div class="row">
+                            <div class="col-md-3 ikona">
+                                <a href="">
+                                <div class="ikonospav">
+                                    <img src="/images/img/button/10.png" alt="">
+                                </div>
+                                <div>
+                                    <h5>Gyvūnėliai</h5>
+                                </div>
+                                </a>
+                                </a>
+                            </div>
+                            <div class="col-md-3 ikona">
+                            <a href="">
+                                <div class="ikonospav">
+                                    <img src="/images/img/button/4.png" alt="">
+                                </div>
+                                <div>
+                                    <h5>Maistas</h5>
+                                </div>
+                                </a>
+                            </div>
+                            <div class="col-md-3 ikona">
+                            <a href="">
+                                <div class="ikonospav">
+                                    <img src="/images/img/button/7.png" alt="">
+                                </div>
+                                <div>
+                                    <h5>Tualetai</h5>
+                                </div>
+                                </a>
+                        </div>
+                    </div>
 
-    <div class="">
-        <h1>Prekės šunims</h1>
-    </div>
+                    <div class="row">
+                        <div class="col-md-3 ikona">
+                        <a href="">
+                            <div class="ikonospav">
+                                <img src="/images/img/button/8.png" alt="">
+                            </div>
+                            <div>
+                                <h5>Žaislai</h5>
+                            </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 ikona">
+                        <a href="">
+                            <div class="ikonospav">
+                                <img src="/images/img/button/14.png" alt="">
+                            </div>
+                            <div>
+                                <h5>Aksesuarai</h5>
+                            </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 ikona">
+                        <a href="">
+                            <div class="ikonospav">
+                                <img src="/images/img/button/9.png" alt="">
+                            </div>
+                            <div>
+                                <h5>Guoliai</h5>
+                            </div>
+                        </a>
+                        </div>
+                    </div>
+                </div>
 
-    <div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="">
-                    <img src="/images/img/button/10.png" alt="">
-                </div>
-                <div>
-                    <h5>Gyvunėliai</h5>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="">
-                    <img src="/images/img/button/4.png" alt="">
-                </div>
-                <div>
-                    <h5>Maistas</h5>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="">
-                    <img src="/images/img/button/7.png" alt="">
-                </div>
-                <div>
-                    <h5>Tualetai</h5>
-                </div>
-            </div>
         </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <div class="">
-                    <img src="/images/img/button/8.png" alt="">
-                </div>
-                <div>
-                    <h5>Žaislai</h5>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="">
-                    <img src="/images/img/button/14.png" alt="">
-                </div>
-                <div>
-                    <h5>Aksesuarai</h5>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="">
-                    <img src="/images/img/button/9.png" alt="">
-                </div>
-                <div>
-                    <h5>Guoliai</h5>
-                </div>
-            </div>
-        </div>
     </div>
-
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+                <div>
+                    <img class="homepaper" src="/images/img/home/wallpaper.png" alt="">
+                    <div class="paperwrite">
+                        <h5>Namai nėra NAMAI be gyvūnų </h5>
+                        <h2> be GYVŪNŲ </h2>
+                        </div>
+                </div>
+                <div class= "rudasfonas">
+                    <div class="topatitraukimas">
+                        <h2>Zoo Prekės</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
