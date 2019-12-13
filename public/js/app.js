2b68608085835e15d02865394ed34c3853b1c747
cozy8 <ingiukasgudz@gmail.com>
2019-12-13 11:39:24 +0200

    xd

--- public/js/app.js
+++ public/js/app.js
@@ -49506,8 +49506,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\xd\zoo4you\zoo4you\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\xd\zoo4you\zoo4you\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\Users\xd\zoo4you\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\Users\xd\zoo4you\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -4,34 +4,44 @@
 
     {!! Form::open(['action' => 'BlogController@store', 'files' => true]) !!}
 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <div class="topatitraukimasnav">
 
-    <div class="form-group">
-        {{Form::label('title', 'Pavadinimas')}}
-        {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Pavadinimas'])}}
-    </div>
-    <div class="form-group">
-        {{Form::label('category', 'Skelbimo kategorija')}}
-        {{Form::select('category', ['pets' => 'Gyvunėliai', 'foods' => 'Maistas','toilets' => 'Tualetai','toys' => 'Žaislai','akse' => 'Aksesuarai','beds' => 'Guoliai'], ['class' => 'form-control'])}}
-    </div>
-    <div class="form-group">
-        {{Form::label('description', 'Aprašymas')}}
-        {{Form::textarea('description', '', ['class' => 'form-control', 'placeholder' => 'Aprašymas'])}}
-    </div>
-    <div class="form-group">
-        {{Form::label('location', 'Vieta')}}
-        {{Form::text('location', '', ['class' => 'form-control', 'placeholder' => 'Vieta'])}}
-    </div>
-    <div class="form-group">
-        {{Form::label('photo', 'Nuotrauka')}}
-        {{Form::file('photo')}}
-    </div>
-    <div class="form-group">
-        {{Form::label('price', 'Kaina')}}
-        {{Form::number('price', '', ['class' => 'form-control', 'placeholder' => 'Kaina', 'step' => '0.01'])}}
+                    <div class="form-group">
+                        {{Form::label('title', 'Pavadinimas')}}
+                        {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Pavadinimas'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('category', 'Skelbimo kategorija')}}
+                        {{Form::select('category', ['lost' => 'Dingo', 'gifting' => 'Dovanoju','adoption' => 'Ieško namų','trade' => 'Keičiu','wandering' => 'Klaidžioja','sell' => 'Parduodu', 'buy' => 'Perku', 'service' => 'Paslaugos', 'found' => 'Rasta', 'various' => 'Įvairūs',], ['class' => 'form-control'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('description', 'Aprašymas')}}
+                        {{Form::textarea('description', '', ['class' => 'form-control', 'placeholder' => 'Aprašymas'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('location', 'Vieta')}}
+                        {{Form::text('location', '', ['class' => 'form-control', 'placeholder' => 'Vieta'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('photo', 'Nuotrauka')}}
+                        {{Form::file('photo')}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('price', 'Kaina')}}
+                        {{Form::number('price', '', ['class' => 'form-control', 'placeholder' => 'Kaina', 'step' => '0.01'])}}
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     </div>
 
 
-    {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+    <div class="aligncenter"> {{Form::submit('Pridėti skelbimą', ['class' => 'btn btn-primary'])}} </div>
     {!! Form::close() !!}
 
 
