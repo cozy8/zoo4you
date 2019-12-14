2bb57aae86a95ee874031a06eb1171fc04cb1251
cozy8 <ingiukasgudz@gmail.com>
2019-12-14 20:26:31 +0200

    xdd

--- resources/views/dog/show.blade.php
+++ resources/views/dog/show.blade.php
@@ -2,11 +2,77 @@
 
 @section('content')
 
-<p>{{$dog->item_name}} </p>
-<p>{{$dog->category}} </p>
-<p>{{$dog->description}} </p>
-<p>{{$dog->dimensions}} </p>
-<p>{{$dog->quantity}} </p>
-<p>{{$dog->price}} </p>
 
-<a href="{{ URL::previous() }}">Atgal</a>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10 topatitraukimasnav">
+        
+            <div class= "rudasfonas">
+                <div class="topatitraukimas">
+                            <h2> {{$dog->item_name}} </h2>
+                </div>
+            </div>
+
+            <div class="topatitraukimasdidelis">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div>
+                                <img class="produktopav" src="/images/img/skelbimai/2.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <h5>
+                                Aprašymas
+                            </h5>
+                            <div class="rudasfonas produktas">
+                                <div class="col-md-12 topatitraukimas ">
+                                    <p class="categorytitle">{{$dog->category}} </p>
+                                    <p class="topatitraukimas">{{$dog->description}} </p>
+                                </div>
+                            </div>
+                                                    
+                            <div class="col-md-10 sellrowapacioje">
+
+                            <div class=" col-md-12 row">
+                                    <div class="col-md-2 sellrow"> <p>Kaina: </p> </div>
+                                    <div class="col-md-5 sellrow"> <p>Išmatavimai: </p> </div>
+                                    <div class="col-md-2 sellrow"> <p>Kiekis: </p> </div>
+                                </div>
+
+                                <div class=" col-md-12 row">
+                                    <div class="col-md-2 rudasfonas sellrow"> <p>{{$dog->price}} </p> </div>
+                                    <div class="col-md-5 rudasfonas  sellrow"> <p>{{$dog->dimensions}} </p> </div>
+                                    <div class="col-md-2 sellrow"> <input style="width: 100%;" type="number" name="quantity" min="1" max="5"> </div>
+                                    <div class="col-md-2 rudasfonas  sellrow"> <img class="cartimg"src="/images/img/icone/bag.png"></div>
+                                </div>
+
+                            </div>
+                                <!--<p>skelbimo data</p>
+                                <p>Miestas</p>
+                                <p>Peržiūros</p>
+                                <p>Kaina</p>-->
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+           <!-- <p>{{$dog->item_name}} </p>
+            <p>{{$dog->category}} </p>
+            <p>{{$dog->description}} </p>
+            <p>{{$dog->dimensions}} </p>
+            <p>{{$dog->quantity}} </p>
+            <p>{{$dog->price}} </p> -->
+
+
+            <a href="{{ URL::previous() }}">Atgal</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
