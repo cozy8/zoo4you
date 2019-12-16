388489775cf23de8d955dc4090cdd52c02f1da8d
Mantas1A <Mantasas15@gmail.com>
2019-12-16 14:20:33 +0200

    nj

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,7 +13,7 @@ class BlogController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->only('create', 'store', 'update', 'edit');
-        $this->middleware('admin')->only('destroy');
+        $this->middleware('admin')->only('destroy', 'confirm', 'setConfirms');
     }
 
 
---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cart;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $cart = new Cart();
+
+
+        $cart->userid = $request->input('id');
+        $cart->category = $request->input('category');
+
+        $cart->quantity = $request->input('quantity');
+
+
+        $cart->save();
+
+        return redirect()->back()->with('success', 'Prekė pridėta');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cart = Cart::where('user_id', $id)->get();
+
+        return view('cart.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cart $cart)
+    {
+        //
+    }
+}
